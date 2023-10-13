71caf30f6f402ef8f8369bb491417713904ff21e
ustinova <rykova.tanya.96@gmail.com>
2023-10-13 13:43:19 +0300

    Add class order

---
+++ online hardware store/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_hardware_store
+{
+    internal class Order
+    {
+        public string NameCustomers { get; set; }
+        public double DateOfCreation { get; set; }
+        public double OrderPosition{ get;set; }
+        public double NumberOfOrders { get; set; }
+
+        public double GetPrice(int number,int price , int _discountPercentage)
+        {
+            double sum = price * number;
+
+            if (number >= 5)
+            {
+                double discount = _discountPercentage * sum;
+
+                return sum - discount;
+            }
+
+            return sum;
+        }
+
+
+    }
+}
