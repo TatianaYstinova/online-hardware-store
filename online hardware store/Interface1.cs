d99c4f980c40c53753c6a35ecdc88a360e154871
ustinova <rykova.tanya.96@gmail.com>
2023-10-24 17:01:11 +0300

    Add interface

---
+++ online hardware store/Interface1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_hardware_store
+{
+    public interface IOrder
+    {
+        public void AddPosition();
+        public double TotalPrice();
+    }
+    public interface ICoods
+    {
+        public string DoInformation();
+        public double GetPrice();
+
+    }
+   
+   
+    
+}
